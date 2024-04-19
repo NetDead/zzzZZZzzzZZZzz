6df7a67d5b488ab1c4b7ec3495abee56d9faca17
o.smirnov <o.smirnov@fun-box.ru>
2024-04-19 19:14:29 +0300

    Initital project setup

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react"
+
+export const App = () => {
+  const [ data, setData ] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async() => {
+      const response = await fetch('api/data', { method: 'GET' });
+
+      const normalizedResponse = await response.json();
+
+      setData(normalizedResponse.data);
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(data);
+
+  return (
+    <>
+      <h1>Hello world</h1>
+      {data.map(({ id, name }) => (<p key={id}>{name}</p>))}
+    </>
+  )
+}
