d25627b3076b51f5f1f4ca40f7a221650be8b2db
o.smirnov <o.smirnov@fun-box.ru>
2024-04-24 16:23:15 +0300

    Refactoring and improvement linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,31 @@
-import eslint from "@eslint/js";
-import tsEslint from 'typescript-eslint';
-// TODO: enable after merging https://github.com/facebook/react/pull/28773
-// import eslintPluginReactHooks from 'eslint-plugin-react-hooks';
+import eslint from '@eslint/js';
+import tslint from 'typescript-eslint';
+import eslintPluginReactHooks from 'eslint-plugin-react-hooks';
 import eslintPluginReactRefresh from 'eslint-plugin-react-refresh';
 import eslintPluginReact from 'eslint-plugin-react';
-import eslintPluginImport from 'eslint-plugin-import';
 import eslintPluginSwitchCase from 'eslint-plugin-switch-case';
 
 export default [
   eslint.configs.recommended,
-  ...tsEslint.configs.recommended,
+  ...tslint.configs.recommended,
+  { ignores: ['public', 'dist', '.storybook'] },
   {
     plugins: {
       'react-refresh': eslintPluginReactRefresh,
-      // TODO: enable after merging https://github.com/facebook/react/pull/28773
-      // 'react-hooks': eslintPluginReactHooks,
+      'react-hooks': eslintPluginReactHooks,
       'switch-case': eslintPluginSwitchCase,
-      react: eslintPluginReact.configs.recommended,
-      import: eslintPluginImport,
+      react: eslintPluginReact,
     },
-    ignores: ['public', 'dist'],
     languageOptions: {
-      parser: tsEslint.parser,
+      parser: tslint.parser,
     },
     rules: {
+      ...eslintPluginReactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
       'no-console': 'error',
-      'import/no-default-export': 'error',
       'arrow-parens': ['error', 'as-needed'],
       'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }],
       'switch-case/newline-between-switch-case': [
